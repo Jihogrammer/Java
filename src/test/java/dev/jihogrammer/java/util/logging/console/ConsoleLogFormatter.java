c0e89a9f7c4b47644985fe49b065548787f44969
jihogrammer <jihogrammer@gmail.com>
2023-08-13 20:13:13 +0900

    refactor ConsoleLogFormatter

--- src/test/java/dev/jihogrammer/java/util/logging/console/ConsoleLogFormatter.java
+++ src/test/java/dev/jihogrammer/java/util/logging/console/ConsoleLogFormatter.java
@@ -1,8 +1,9 @@
 package dev.jihogrammer.java.util.logging.console;
 
 import dev.jihogrammer.java.util.logging.LogFormatter;
-import dev.jihogrammer.java.util.reflection.ClassFinder;
-import dev.jihogrammer.java.util.reflection.FieldFinder;
+import dev.jihogrammer.java.reflect.ClassFinder;
+import dev.jihogrammer.java.reflect.FieldFinder;
+import org.slf4j.Logger;
 import org.slf4j.event.Level;
 
 import java.lang.reflect.Modifier;
@@ -10,35 +11,55 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ConsoleLogFormatter implements LogFormatter {
-    private static final int MAX_NAME_LENGTH = new ClassFinder().findAllClasses().stream()
-            .filter(clazz -> new FieldFinder().hasFieldInClass(clazz, "log"))
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+    private static final int MAX_THREAD_NAME_LENGTH = 19;
+    private static final int MAX_LEVEL_NAME_LENGTH = 5;
+    private static final int MAX_CLASS_NAME_LENGTH = new ClassFinder().findAllClasses().stream()
+            .filter(clazz -> new FieldFinder(clazz).hasField(Logger.class))
             .filter(clazz -> !Modifier.isFinal(clazz.getModifiers()))
             .mapToInt(clazz -> clazz.getSimpleName().length())
             .max()
             .orElse(20);
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
-
     @Override
     public String format(final String name, final Level level, final String message, final Object[] args) {
-        return LocalDateTime.now().format(DATE_TIME_FORMATTER) + ' ' +
-                '[' + formatWithLength(Thread.currentThread().getName(), 19) + ']' + ' ' +
-                '[' + formatWithLength(level.name(), 5) + ']' + ' ' +
-                '[' + formatWithLength(name, MAX_NAME_LENGTH) + ']' + ' ' +
-                formatMessageBody(message, args);
+        return "%s [%s] [%s] [%s] %s".formatted(timestamp(), threadName(), levelName(level), className(name), messageBody(message, args));
+    }
+
+    private String timestamp() {
+        return LocalDateTime.now().format(DATE_TIME_FORMATTER);
+    }
+
+    private String threadName() {
+        return formatWithLength(Thread.currentThread().getName(), MAX_THREAD_NAME_LENGTH);
+    }
+
+    private String levelName(final Level level) {
+        return formatWithLength(level.name(), MAX_LEVEL_NAME_LENGTH);
+    }
+
+    private String className(String name) {
+        return formatWithLength(name, MAX_CLASS_NAME_LENGTH);
     }
 
     private String formatWithLength(final String str, final int length) {
         return String.format("%" + length + "s", str.substring(Math.max(0, str.length() - length)));
     }
 
-    private String formatMessageBody(final String message, final Object[] args) {
+    private String messageBody(final String message, final Object[] args) {
         if (message.contains("{}")) {
             StringBuilder sb = new StringBuilder();
             String[] tokens = message.split("\\{}");
-            for (int index = 0; index < Math.min(tokens.length, args.length); index++) {
-                sb.append(tokens[index]).append(args[index].toString());
+
+            int index = 0;
+            while (index < tokens.length) {
+                sb.append(tokens[index]);
+                if (index < args.length) {
+                    sb.append(args[index].toString());
+                }
+                index++;
             }
+
             return sb.toString();
         }
         return message;
