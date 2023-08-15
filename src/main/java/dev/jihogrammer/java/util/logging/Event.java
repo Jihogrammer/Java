6b05fd8b82221e60129018d4621b2425bcfdd173
jihogrammer <jihogrammer@gmail.com>
2023-08-15 19:17:48 +0900

    update Event constructor

--- src/main/java/dev/jihogrammer/java/util/logging/Event.java
+++ src/main/java/dev/jihogrammer/java/util/logging/Event.java
@@ -1,26 +1,22 @@
 package dev.jihogrammer.java.util.logging;
 
-import java.util.Arrays;
+import lombok.Getter;
 
+@Getter
 public class Event {
     private final Timestamp timestamp;
-    private final Level level;
     private final Thread thread;
+    private final Level level;
     private final String name;
     private final String format;
     private final Object[] args;
 
-    public Event(Level level, Thread thread, String name, String format, Object... args) {
-        this.timestamp = Timestamp.now();
-        this.level = level;
+    public Event(Timestamp timestamp, Thread thread, Level level, String name, String format, Object... args) {
+        this.timestamp = timestamp;
         this.thread = thread;
+        this.level = level;
         this.name = name;
         this.format = format;
         this.args = args;
-    }
-
-    @Override
-    public String toString() {
-        return "%s [%s] [%5.5s] [%s] %s %s".formatted(timestamp, thread.getName(), level, name, format, Arrays.toString(args));
     }
 }
